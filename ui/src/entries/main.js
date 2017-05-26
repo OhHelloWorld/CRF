7bd2d056948257c6897ae968c15354a836351047
OhHelloWorld <18255629607@163.com>
2017-05-26 17:00:50 +0800

    更新四诊信息

--- ui/src/entries/main.js
+++ ui/src/entries/main.js
@@ -4,12 +4,13 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../lib/css/css/AdminLTE.min.css';
 import '../lib/css/css/skins/skin-blue.min.css';
 
-
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import '../modules/home.js';
 import '../modules/fourDiagnostic.js';
-var main = angular.module('main', [uiRouter, 'home','fourDiagnostic','patientInfo']);
+import '../modules/patientInfo.js';
+
+var main = angular.module('main', [uiRouter, 'home', 'fourDiagnostic', 'patientInfo']);
 
 main.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
--- ui/src/modules/fourDiagnostic.js
+++ ui/src/modules/fourDiagnostic.js
@@ -2,5 +2,37 @@ import angular from 'angular';
 
 angular.module('fourDiagnostic', [])
   .controller('fourDiagnosticController', ['$scope', '$http', function($scope, $http) {
-    
+    $scope.save = function() {
+      if (($scope.fatigue && $scope.skinItching && $scope.twoEyesDry &&
+          $scope.blurredVision && $scope.depression && $scope.irritability &&
+          $scope.insomnia && $scope.easyWakeUp && $scope.tinnitus && $scope.dryMouth && $scope.mouthPain &&
+          $scope.badBreath && $scope.nausea && $scope.belching && $scope.abdominalDistention && $scope.flankPainStinging &&
+          $scope.flankPainSwell && $scope.flankPainDull && $scope.flankPainDiscomfort && $scope.anorexia && $scope.aphrodisiacCold &&
+          $scope.limb && $scope.backacheFootSoft && $scope.handFootFanHot && $scope.urineYellow && $scope.constipation && $scope.looseStools &&
+          $scope.perspiration && $scope.nightSweats && $scope.lowerExtremityEdema && $scope.faceDull && $scope.eyeYellow && $scope.bodyYellow &&
+          $scope.spiderNevus && $scope.liverPalm && $scope.abdominalVeins && $scope.yellowTumor) != undefined) {
+        $http({
+          method: 'POST',
+          url: '/api/fourDiagnosticInfor'
+        }).then(function success() {
+          $scope.information = '保存成功！';
+          $('#infoModal').modal({
+            keyboard: true
+          });
+        }, function fail() {
+          $scope.information = '保存失败！';
+          $('#infoModal').modal({
+            keyboard: true
+          });
+        });
+      } else {
+        $scope.confirmInfo = '填写尚不完整，是否保存？';
+        $('#confirmModal').modal({
+          keyboard: true
+        });
+      }
+    };
+    $scope.confirm = function() {
+
+    };
   }]);
