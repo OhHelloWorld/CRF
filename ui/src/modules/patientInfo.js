245660728a483ce6fc576db1027b325d7a9be8a9
renxiangjie <1021031476@qq.com>
2017-05-26 15:59:51 +0800

    xxxxxxxx

--- ui/src/modules/patientInfo.js
+++ ui/src/modules/patientInfo.js
@@ -6,4 +6,21 @@ var patientInfo = angular.module('patientInfo', []);
 
 patientInfo.controller('patientInfoController', ['$scope', '$http', function($scope, $http) {
 
+
+  $('#datepicker').datepicker({
+    autoclose: true
+  });
+
+  $('#datepicker2').datepicker({
+    autoclose: true
+  });
+
+ 
+  //新建
+  $http({
+    method:'POST',
+    url:''
+  }).then(function(){
+    alert('post successed!!');
+  });
 }]);
