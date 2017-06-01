cbb31d0a19f11c58777084c507bb1d729d0c6921
OhHelloWorld <18255629607@163.com>
2017-06-01 09:12:21 +0800

    tonguePulse

--- ui/src/modules/tonguePulse.js
+++ ui/src/modules/tonguePulse.js
@@ -227,225 +227,298 @@ angular.module('tonguePulse', [])
       }
       if (number == 1 && status == true) {
         tonguePulse.tongue += ',淡红';
+        return;
       }
       if (number == 2 && status == true) {
         tonguePulse.tongue += ',淡白';
+        return;
       }
       if (number == 3 && status == true) {
         tonguePulse.tongue += ',淡紫';
+        return;
       }
       if (number == 4 && status == true) {
         tonguePulse.tongue += ',红';
+        return;
       }
       if (number == 5 && status == true) {
         tonguePulse.tongue += ',绛';
+        return;
       }
       if (number == 6 && status == true) {
         tonguePulse.tongue += ',青';
+        return;
       }
       if (number == 7 && status == true) {
         tonguePulse.tongue += ',紫暗';
+        return;
       }
       if (number == 8 && status == true) {
         tonguePulse.tongue += ',瘀点瘀斑';
+        return;
       }
       if (number == 9 && status == true) {
         tonguePulse.tonguePart += ',全舌';
+        return;
       }
       if (number == 10 && status == true) {
         tonguePulse.tonguePart += ',局部';
         tonguePulse.tonguePartialDescription = $scope.tongueDescription;
+        return;
       }
       if (number == 11 && status == true) {
         tonguePulse.tongueBody += ',荣';
+        return;
       }
       if (number == 12 && status == true) {
         tonguePulse.tongueBody += ',枯';
+        return;
       }
       if (number == 13 && status == true) {
         tonguePulse.tongueBody += ',瘦';
+        return;
       }
       if (number == 14 && status == true) {
         tonguePulse.tongueBody += ',胖';
+        return;
       }
       if (number == 15 && status == true) {
         tonguePulse.tongueBody += ',齿痕';
+        return;
       }
       if (number == 16 && status == true) {
         tonguePulse.tongueBody += ',点刺';
+        return;
       }
       if (number == 17 && status == true) {
         tonguePulse.tongueBody += ',裂纹';
+        return;
       }
       if (number == 18 && status == true) {
         tonguePulse.tongueBody += ',舌体瘀斑';
+        return;
       }
       if (number == 19 && status == true) {
         tonguePulse.mossy += ',薄';
+        return;
       }
       if (number == 20 && status == true) {
         tonguePulse.mossy += ',厚';
+        return;
       }
       if (number == 21 && status == true) {
         tonguePulse.mossy += ',润';
+        return;
       }
       if (number == 22 && status == true) {
         tonguePulse.mossy += ',少津';
+        return;
       }
       if (number == 23 && status == true) {
         tonguePulse.mossy += ',燥';
+        return;
       }
       if (number == 24 && status == true) {
         tonguePulse.mossy += ',糙';
+        return;
       }
       if (number == 25 && status == true) {
         tonguePulse.mossy += ',焦';
+        return;
       }
       if (number == 26 && status == true) {
         tonguePulse.mossy += ',枯';
+        return;
       }
       if (number == 27 && status == true) {
         tonguePulse.mossy += ',腻';
+        return;
       }
       if (number == 28 && status == true) {
         tonguePulse.mossy += ',腐';
+        return;
       }
       if (number == 29 && status == true) {
         tonguePulse.mossy += ',剥';
+        return;
       }
       if (number == 30 && status == true) {
         tonguePulse.mossy += ',类剥';
+        return;
       }
       if (number == 31 && status == true) {
         tonguePulse.mossy += ',无苔';
+        return;
       }
       if (number == 32 && status == true) {
         tonguePulse.mossyPart += ',全舌';
+        return;
       }
       if (number == 33 && status == true) {
         tonguePulse.mossyPart += ',局部';
         tonguePulse.mossyPartialDescription = $scope.mossyDes;
+        return;
       }
       if (number == 34 && status == true) {
         tonguePulse.tongueColor += ',白';
+        return;
       }
       if (number == 35 && status == true) {
         tonguePulse.tongueColor += ',略黄';
+        return;
       }
       if (number == 36 && status == true) {
         tonguePulse.tongueColor += ',黄';
+        return;
       }
       if (number == 37 && status == true) {
         tonguePulse.tongueColor += ',灰';
+        return;
       }
       if (number == 38 && status == true) {
         tonguePulse.tongueColor += ',黑';
+        return;
       }
       if (number == 39 && status == true) {
         tonguePulse.tongueColorPart += ',全舌';
+        return;
       }
       if (number == 40 && status == true) {
         tonguePulse.tongueColorPart += ',局部';
         tonguePulse.tongueColorPartialDescription = $scope.tongueColorDes;
+        return;
       }
       if (number == 41 && status == true) {
         tonguePulse.sublingualVaricoseVeins += ',正常';
+        return;
       }
       if (number == 42 && status == true) {
         tonguePulse.sublingualVaricoseVeins += ',轻微';
+        return;
       }
       if (number == 43 && status == true) {
         tonguePulse.sublingualVaricoseVeins += ',明显';
+        return;
       }
       if (number == 44 && status == true) {
         tonguePulse.leftPulse += ',浮';
+        return;
       }
       if (number == 45 && status == true) {
         tonguePulse.leftPulse += ',沉';
+        return;
       }
       if (number == 46 && status == true) {
         tonguePulse.leftPulse += ',弦';
+        return;
       }
       if (number == 47 && status == true) {
         tonguePulse.leftPulse += ',滑';
+        return;
       }
       if (number == 48 && status == true) {
         tonguePulse.leftPulse += ',细';
+        return;
       }
       if (number == 49 && status == true) {
         tonguePulse.leftPulse += ',数';
+        return;
       }
       if (number == 50 && status == true) {
         tonguePulse.leftPulse += ',濡';
+        return;
       }
       if (number == 51 && status == true) {
         tonguePulse.leftPulse += ',缓';
+        return;
       }
       if (number == 52 && status == true) {
         tonguePulse.leftPulse += ',涩';
+        return;
       }
       if (number == 53 && status == true) {
         tonguePulse.leftPulse += ',迟';
+        return;
       }
       if (number == 54 && status == true) {
         tonguePulse.leftPulse += ',长';
+        return;
       }
       if (number == 55 && status == true) {
         tonguePulse.leftPulse += ',短';
+        return;
       }
       if (number == 56 && status == true) {
         tonguePulse.leftPulse += ',虚';
+        return;
       }
       if (number == 57 && status == true) {
         tonguePulse.leftPulse += ',弱';
+        return;
       }
       if (number == 58 && status == true) {
         tonguePulse.leftPulse += ',结代';
+        return;
       }
       if (number == 59 && status == true) {
         tonguePulse.rightPulse += ',浮';
+        return;
       }
       if (number == 60 && status == true) {
         tonguePulse.rightPulse += ',沉';
+        return;
       }
       if (number == 61 && status == true) {
         tonguePulse.rightPulse += ',弦';
+        return;
       }
       if (number == 62 && status == true) {
         tonguePulse.rightPulse += ',滑';
+        return;
       }
       if (number == 63 && status == true) {
         tonguePulse.rightPulse += ',细';
+        return;
       }
       if (number == 64 && status == true) {
         tonguePulse.rightPulse += ',数';
+        return;
       }
       if (number == 65 && status == true) {
         tonguePulse.rightPulse += ',濡';
+        return;
       }
       if (number == 66 && status == true) {
         tonguePulse.rightPulse += ',缓';
+        return;
       }
       if (number == 67 && status == true) {
         tonguePulse.rightPulse += ',涩';
+        return;
       }
       if (number == 68 && status == true) {
         tonguePulse.rightPulse += ',迟';
+        return;
       }
       if (number == 69 && status == true) {
         tonguePulse.rightPulse += ',长';
+        return;
       }
       if (number == 70 && status == true) {
         tonguePulse.rightPulse += ',短';
+        return;
       }
       if (number == 71 && status == true) {
         tonguePulse.rightPulse += ',虚';
+        return;
       }
       if (number == 72 && status == true) {
         tonguePulse.rightPulse += ',弱';
+        return;
       }
       if (number == 73 && status == true) {
         tonguePulse.rightPulse += ',结代';
+        return;
       }
     }
   }]);
