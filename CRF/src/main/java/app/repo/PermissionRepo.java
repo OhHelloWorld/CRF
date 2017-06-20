a43349a619a9560add6f1bc5bede3ee49f1bdefc
songtianjian <524000659@qq.com>
2017-06-20 18:36:12 +0800

    权限添加

--- CRF/src/main/java/app/repo/PermissionRepo.java
+++ CRF/src/main/java/app/repo/PermissionRepo.java
@@ -7,4 +7,12 @@ import org.springframework.data.repository.CrudRepository;
  * Created by 52400 on 2017/6/20.
  */
 public interface PermissionRepo extends CrudRepository<PermissionDO, Long>{
+
+    
+
+
+
+
+
+
 }
