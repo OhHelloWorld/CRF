613ff1812cd7a2f00f51fe541d7d1f738ed0ce3f
songtianjian <524000659@qq.com>
2017-07-03 11:38:23 +0800

    添加消息接口

--- CRF/src/main/java/app/repo/MessageRepo.java
+++ CRF/src/main/java/app/repo/MessageRepo.java
@@ -1,14 +1,21 @@
 package app.repo;
 
 import app.entities.MessageDO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
 /**
  * Created by 52400 on 2017/6/30.
  */
-public interface MessageRepo extends CrudRepository<MessageDO, Long> {
+public interface MessageRepo extends PagingAndSortingRepository<MessageDO, Long> {
 
-    List<MessageDO> findByReceivedUserId(Long userId);
+    Page<MessageDO> findByReceivedUserId(Pageable pageable, Long userId);
+
+    @Query(value = "select * from message where received_user_id = ?1 and readed = 0", nativeQuery = true)
+    List<MessageDO> findNotRead(Long userId);
 }
--- CRF/src/main/java/app/serviceImpl/MessageServiceImpl.java
+++ CRF/src/main/java/app/serviceImpl/MessageServiceImpl.java
@@ -3,10 +3,13 @@ package app.serviceImpl;
 import app.Utils.ConvertUtil;
 import app.Utils.UserMsgTool;
 import app.dto.MessageDTO;
+import app.dto.PageDTO;
 import app.entities.MessageDO;
 import app.repo.MessageRepo;
 import app.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -28,18 +31,34 @@ public class MessageServiceImpl implements MessageService {
     @Autowired
     private ConvertUtil convertUtil;
 
-    public List<MessageDTO> getCurrentUserMessage() {
-        List<MessageDTO> messageDTOList = new ArrayList<>();
-        for(MessageDO m : messageRepo.findByReceivedUserId(userMsgTool.getCurrentUserId())) {
-            messageDTOList.add(convertUtil.convertToMessageDTO(m));
-        }
-        return messageDTOList;
+    public PageDTO<MessageDTO> getCurrentUserMessage(Pageable pageable) {
+        return getPageDTO(messageRepo.findByReceivedUserId(pageable, userMsgTool.getCurrentUserId()));
     }
 
     public void readMessage(Long messageId) {
         MessageDO messageDO = messageRepo.findOne(messageId);
         messageDO.setRead(true);
         messageRepo.save(messageDO);
+    }
+
+    @Override
+    public List<MessageDTO> getNotReadMessage() {
+        List<MessageDTO> messageDTOList = new ArrayList<>();
+        for(MessageDO m : messageRepo.findNotRead(userMsgTool.getCurrentUserId())) {
+            messageDTOList.add(convertUtil.convertToMessageDTO(m));
+        }
+        return messageDTOList;
+    }
+
+    public PageDTO<MessageDTO> getPageDTO(Page<MessageDO> messageDOS) {
+        PageDTO<MessageDTO> pageDTO = new PageDTO<>();
+        List<MessageDTO> messageDTOList = new ArrayList<>();
+        for(MessageDO m : messageDOS.getContent()) {
+            messageDTOList.add(convertUtil.convertToMessageDTO(m));
+        }
+        pageDTO.setContent(messageDTOList);
+        pageDTO.setTotalNumber(messageDOS.getTotalPages());
+        return pageDTO;
     }
 
 }
