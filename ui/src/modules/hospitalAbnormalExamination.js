09c6fb976f9e4e87071c49ff1827b72505ae393d
renxiangjie <1021031476@qq.com>
2017-09-14 09:28:12 +0800

    update

--- ui/src/modules/hospitalAbnormalExamination.js
+++ ui/src/modules/hospitalAbnormalExamination.js
@@ -18,16 +18,16 @@ angular.module('hospitalAbnormalExamination', [])
       allData = response.data;
       
       //计算页数
-      for (var a = 1; a <= response.data.length / 10; a++) {
+      for (var a = 1; a <= response.data.length / 5; a++) {
         $scope.pageNums.push(a);
       }
-      if (response.data.length % 10 != 0) {
-        $scope.pageNums.push(Math.ceil(response.data.length / 10));
+      if (response.data.length % 5 != 0) {
+        $scope.pageNums.push(Math.ceil(response.data.length / 5));
       }
 
       //设置默认显示的数据
-      if(response.data.length >= 10){
-        for(var b = 0; b < 10; b++) {
+      if(response.data.length >= 5){
+        for(var b = 0; b < 5; b++) {
           $scope.mlfaes.push(response.data[b]);
         }
       }else{
@@ -41,7 +41,7 @@ angular.module('hospitalAbnormalExamination', [])
     //根据页数显示数据
     $scope.testFun = function(num) {
       $scope.mlfaes.length = 0;
-      for(var a=(num-1)*10+1, b = 0; a<=num*10&&a<=allData.length&&b<10; a++, b++){
+      for(var a=(num-1)*5+1, b = 0; a<=num*5&&a<=allData.length&&b<5; a++, b++){
         $scope.mlfaes.push(allData[a-1]);
       }
     };
